8cf9a45feb0ce9b7189b3495cbcd8c417526aa00
Roman-Labtech <labtechno.roman@yandex.ru>
2017-08-02 17:33:14 +0300

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -18,7 +18,14 @@ myApp.onPageInit('about', function (page) {
     });
 });
 
-
+function onDeviceReady() {
+        // Register the event listener
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    };
+    function onBackKeyDown() {
+    
+    alert("123");
+    };
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
