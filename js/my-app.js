a5ea8d3bb817d292a1e205a24bae88fab470d8a3
Roman-Labtech <labtechno.roman@yandex.ru>
2017-08-02 09:50:15 +0300

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -229,7 +229,7 @@ function countPow(n) {
 
 function phonecall()
 {
-    window.plugins.CallNumber.callNumber(onSuccess, onError, +79200125632, true);
+    window.plugins.CallNumber.callNumber(onSuccess, onError, +78313367613, true);
 }
 
 function onSuccess(result){
@@ -255,6 +255,37 @@ function onBackKeyDown(e) {
 
 
 
- 
+function buttonweb(){
+  document.getElementById("openBrowser").addEventListener("click", openBrowser);  
+}
 
+
+
+function openBrowser() {
+   var url = 'https://cordova.apache.org';
+   var target = '_blank';
+   var options = "location = yes"
+   var ref = cordova.InAppBrowser.open(url, target, options);
+   
+   ref.addEventListener('loadstart', loadstartCallback);
+   ref.addEventListener('loadstop', loadstopCallback);
+   ref.addEventListener('loadloaderror', loaderrorCallback);
+   ref.addEventListener('exit', exitCallback);
+
+   function loadstartCallback(event) {
+      console.log('Loading started: '  + event.url)
+   }
+
+   function loadstopCallback(event) {
+      console.log('Loading finished: ' + event.url)
+   }
+
+   function loaderrorCallback(error) {
+      console.log('Loading error: ' + error.message)
+   }
+
+   function exitCallback() {
+      console.log('Browser is closed...')
+   }
+}
  
